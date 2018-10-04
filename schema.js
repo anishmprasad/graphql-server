0ed0a8c9ea5e6b45f9f48fce0357ab17a9db338c
anish.m.prasad <anish.m.prasad@gmail.com>
2018-10-04 16:12:11 +0530

    axios on json-server

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const {
   GraphQLString,
   GraphQLObjectType,
@@ -9,16 +10,16 @@ const {
 
 // Hardcoded customer data
 
-const customers  = [
-  {id : "1" , name : "anish" , email:"anishmprasad@gmail.com",age :28},
-  { id: "2", name: "nija", email: "nijabhagyanath@gmail.com", age: 28 },
-  { id: "3", name: "nisha", email: "nishapriyajith@gmail.com", age: 35 },
-  { id: "4", name: "adarsh", email: "adarsh@gmail.com", age: 10 },
-  { id: "5", name: "priyajith", email: "priyajith@gmail.com", age: 45 },
-  { id: "6", name: "sunitha", email: "sunitha@gmail.com", age: 58 },
-  { id: "7", name: "prasad", email: "prasad@gmail.com", age: 62 },
+// const customers  = [
+//   {id : "1" , name : "anish" , email:"anishmprasad@gmail.com",age :28},
+//   { id: "2", name: "nija", email: "nijabhagyanath@gmail.com", age: 28 },
+//   { id: "3", name: "nisha", email: "nishapriyajith@gmail.com", age: 35 },
+//   { id: "4", name: "adarsh", email: "adarsh@gmail.com", age: 10 },
+//   { id: "5", name: "priyajith", email: "priyajith@gmail.com", age: 45 },
+//   { id: "6", name: "sunitha", email: "sunitha@gmail.com", age: 58 },
+//   { id: "7", name: "prasad", email: "prasad@gmail.com", age: 62 },
   
-]
+// ]
 // CustomerType
 
 const CustomerType = new GraphQLObjectType({
@@ -40,17 +41,15 @@ const RootQuery = new GraphQLObjectType({
         id: { type :GraphQLString }
       },
       resolve(parentValue, args) {
-        for (let i = 0; i < customers.length; i += 1) {
-          if (customers[i].id == args.id) {
-            return customers[i]
-          }
-        }
+        return axios.get('http://localhost:3000/customers' + args.id)
+        .then(res => res.data )
       }
     },
     customers:{
       type: new GraphQLList(CustomerType),
       resolve(parentValue,args) {
-        return customers
+        return axios.get('http://localhost:3000/customers')
+          .then(res => res.data)
       }
     }
   }
